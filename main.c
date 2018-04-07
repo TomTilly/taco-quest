cf5937b1c04d8e0329cb45aae82b1062baf18b8d
xiphiasnonus <foster.pianist@gmail.com>
2018-04-06 23:49:55 -0400

    Basic ncurses testq

--- main.c
+++ main.c
@@ -1,7 +1,53 @@
-#include <stdio.h>
+#include <ncurses.h>
 
 int main() 
 {
-    printf("hello taco\n");
-    return 0;
+	int max_x, max_y;
+	
+	initscr(); 		  // ncurses takes over!
+	noecho();  		  // no display user typing!!
+	curs_set(false);  // don't show cursor
+	
+	getmaxyx(stdscr, max_y, max_x);
+	
+	clear();		  // clear screen
+	
+	// move, print, and refresh so it appears
+	int y = 5;
+	int x = 5;
+	mvprintw(y, x, "O");
+	refresh();
+	
+	int key;
+	do
+	{
+		key = getch();
+		clear();
+		
+		if (key == 'd' && x < (max_x - 1))
+		{
+			x++;
+		}
+		if (key == 'a' && x > 0)
+		{
+			x--;
+		}
+		if (key == 's' && y < (max_y - 1))
+		{
+			y++;
+		}
+		if (key == 'w' && y > 0)
+		{
+			y--;
+		}
+		
+		mvprintw(y,x,"O");
+		refresh();
+		
+	} while (key != 'q');
+		
+	endwin();  // clean up
+	
+	return 0;
 }
+
