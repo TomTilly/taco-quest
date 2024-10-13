9040c2347576c4ef319ded80e38b002f244eb5b4
Tom Tillistrand <tomtillistrand@gmail.com>
2024-10-13 18:42:16 -0400

    Create window, build script, design doc

---
+++ design.md
@@ -0,0 +1,15 @@
+# Taco Quest Game Design
+
+## Summary
+
+Classic snake game with a multiplayer twist...and tacos!
+
+- Each player controls a snake in an arena/level
+- Snakes fight for tacos that spawn
+- When a snake eats a taco, it grows longer
+
+## Ideas
+
+- Koopa shell-esque item to slow down other player
+- Each player can bite the other player's tail and sever it
+- Players can pass underneath their own tail when moving perpendicular to it
--- main.c
+++ main.c
@@ -1,7 +1,43 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
+#include "SDL2/SDL.h"
 
-int main() 
-{
+int32_t main () {
     printf("hello taco\n");
+
+    int rc = SDL_Init(SDL_INIT_EVERYTHING);
+    if (rc < 0) {
+        printf("SDL_Init failed %s\n", SDL_GetError());
+        return 1;
+    }
+
+    const char* title = "Taco Quest";
+    SDL_Window* window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    if (window == NULL) {
+        printf("SDL_CreateWindow failed %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+                break;
+            }
+        }
+
+        SDL_FillRect(window_surface, NULL, SDL_MapRGB(window_surface->format, 0x00, 0x00, 0x00));
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(0);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
