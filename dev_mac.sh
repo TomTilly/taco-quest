011b047da4ceb0c06c7dec726ae192c5ad9311ce
Thomas Foster <foster.pianist@gmail.com>
2024-10-26 16:47:24 -0400

    remove unneeded include and lib path flags

--- dev_mac.sh
+++ dev_mac.sh
@@ -5,7 +5,7 @@ set -e
 set -x
 
 # Compile program
-clang -std=c11 main.c -Wall -Wextra -Werror -I /usr/local/Cellar/sdl2/2.0.16/include -L /usr/local/Cellar/sdl2/2.0.16/lib -l SDL2 -g -o taco-quest
+clang -std=c11 main.c -Wall -Wextra -Werror -lSDL2 -g -o taco-quest
 
 # Run program
 ./taco-quest
